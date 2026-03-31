8eb5dd6ae57bf72c448c24f165b4bb8b6ebce6c7
Amontanez <lambartecnology@gmail.com>
2026-03-30 21:47:50 -0500

    add 1

--- src/test/java/tests/PrimerTest.java
+++ src/test/java/tests/PrimerTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,14 +9,21 @@ import org.testng.annotations.Test;
 
 public class PrimerTest {
     @Test
-    public void abrirGoogle() {
+    public void seleccionarCategoria() throws InterruptedException {
+
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com");
-        System.out.println("Página abierta correctamente");
-        driver.quit();
+        driver.get("https://www.demoblaze.com");
 
-        }
+        // Click en "Laptops"
+        driver.findElement(By.linkText("Laptops")).click();
+        Thread.sleep(2000); // Espera 2 segundos
+        driver.findElement(By.linkText("Phones")).click();
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
 
 }
 
